7b4129590c002a75b9557a02f87651045290c4a5
unknown <carolinedasmerces@gmail.com>
2020-09-08 13:17:33 -0300

    Lista 1

---
+++ primeiraAula/Ex4ValorD/src/ValorD.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ValorD {
+
+	public static void main(String[] args) {
+		/* Escreva um sistema que leia três números inteiros e positivos (A, B, C) e
+		calcule a seguinte expressão: D=(R+S)/2, onde R=(A+B)^2 e S=(B+C)^2
+				*/
+		
+		
+		double r, d, s, p=2;
+		int a, b, c;
+		Scanner ler = new Scanner(System.in);
+		System.out.print("Digite o valor de A: ");
+		a = ler.nextInt();
+		System.out.print("Digite o valor de B: ");
+		b = ler.nextInt();
+		System.out.print("Digite o valor de C: ");
+		c = ler.nextInt();
+		r = (a+b);
+		//Math.pow(r, p);
+		r = r*r;
+		s = (b+c);
+		s = s*s;
+		d = ((r+s)/2);
+		System.out.println("O valor de D é: "+d);
+		
+		ler.close();
+		}
+
+}
